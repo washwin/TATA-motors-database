ae8532f45b6b1c285c28d37c78ac0dc33f649c4b
washwin <114923029+washwin@users.noreply.github.com>
2023-10-06 22:53:15 +0530

    checking new feature

--- init.py
+++ init.py
@@ -3,10 +3,13 @@ from initialization import createdb
 from initialization import tables
 from initialization import populate
 
-usr = input("Enter username (usually postgres) : ")
-psswd = input("Enter password : ")
+def main():
+    usr = input("Enter username (usually postgres) : ")
+    psswd = input("Enter password : ")
 
-deletedb.main(usr,psswd)
-createdb.main(usr,psswd)
-tables.main(usr,psswd)
-populate.main(usr,psswd)
+    deletedb.main(usr,psswd)
+    createdb.main(usr,psswd)
+    tables.main(usr,psswd)
+    populate.main(usr,psswd)
+
+main()
