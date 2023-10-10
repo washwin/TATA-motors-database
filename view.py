a43d5e7a255a25990689f86f7c67da7980364827
washwin <114923029+washwin@users.noreply.github.com>
2023-10-10 23:05:04 +0530

    view added

---
+++ view.py
@@ -0,0 +1,54 @@
+import psycopg2
+
+def display():
+    print("SELECT TABLE FOR UPDATION:")
+    print("(1)Client")
+    print("(2)Employee")
+    print("(3)Employee Training")
+    print("(4)Parts Inventory")
+    print("(5)Sales")
+    print("(6)Supplier")
+    print("(7)Vehicle")
+
+def client():
+    sql_query = """SELECT * FROM client;"""
+    return sql_query
+
+def view(usr,psswd):
+    try:
+        with psycopg2.connect(database='tatadb',
+                            host="localhost",
+                            user=usr,
+                            password=psswd,
+                            port=5432) as connection:
+            display()
+            i = input("Choose index : ")
+            update_query = ""
+            match i:
+                case "1": 
+                    sql_query = client()
+                case _:
+                    print("INVALID INPUT!!")
+                    exit()
+
+            with connection.cursor() as cursor:
+                cursor.execute(sql_query)
+                op = cursor.fetchall()
+                for o in op:
+                    print(o)
+                connection.commit()
+                cursor.close()
+        connection.close()
+    except psycopg2.OperationalError as e:
+        print("INVALID CREDENTIALS")
+        # print(e)
+    except psycopg2.Error as e:
+        print(e)
+        connection.rollback()
+
+def main():
+    usr = input("Enter username (usually postgres) : ")
+    psswd = input("Enter password : ")
+    view(usr,psswd)
+
+main()
