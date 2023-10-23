2d29f41ac9e7f8fcebfaf0d97f858b247d64138c
washwin <114923029+washwin@users.noreply.github.com>
2023-10-23 15:54:32 +0530

    view added

--- view.py
+++ view.py
@@ -2,7 +2,7 @@ import psycopg2
 import getpass
 
 def display():
-    print("SELECT TABLE FOR UPDATION:")
+    print("SELECT TABLE TO VIEW:")
     print("(1)Client")
     print("(2)Employee")
     print("(3)Department")
@@ -16,6 +16,30 @@ def display():
 def client():
     sql_query = """SELECT * FROM client;"""
     return sql_query
+def employee():
+    sql_query = """SELECT * FROM employee;"""
+    return sql_query
+def department():
+    sql_query = """SELECT * FROM department;"""
+    return sql_query
+def parts():
+    sql_query = """SELECT * FROM parts_inventory;"""
+    return sql_query
+def sales():
+    sql_query = """SELECT * FROM sales;"""
+    return sql_query
+def supplier():
+    sql_query = """SELECT * FROM supplier;"""
+    return sql_query
+def vehicle():
+    sql_query = """SELECT * FROM vehicle;"""
+    return sql_query
+def model():
+    sql_query = """SELECT * FROM model;"""
+    return sql_query
+def factory():
+    sql_query = """SELECT * FROM factory;"""
+    return sql_query
 
 def view(usr,psswd):
     try:
@@ -26,10 +50,26 @@ def view(usr,psswd):
                             port=5432) as connection:
             display()
             i = input("Choose index : ")
-            update_query = ""
+            sql_query = ""
             match i:
                 case "1": 
                     sql_query = client()
+                case "2": 
+                    sql_query = employee()
+                case "3": 
+                    sql_query = department()
+                case "4": 
+                    sql_query = parts()
+                case "5": 
+                    sql_query = sales()
+                case "6": 
+                    sql_query = supplier()
+                case "7": 
+                    sql_query = vehicle()
+                case "8": 
+                    sql_query = model()
+                case "9": 
+                    sql_query = factory()
                 case _:
                     print("INVALID INPUT!!")
                     exit()
