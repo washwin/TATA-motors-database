c7bf5358fa85775d3a943de46af414502ca4b35a
washwin <114923029+washwin@users.noreply.github.com>
2023-10-27 18:16:35 +0530

    viewgui up and running

--- initialization/createdb.py
+++ initialization/createdb.py
@@ -10,7 +10,12 @@ def main(usr, psswd):
                                 port=5432)
         connection.autocommit = True
         cursor = connection.cursor()
-
+        sql_query = ''' DROP database if exists tatadb '''    
+        cursor.execute(sql_query)
+        sql_query = ''' DROP USER IF EXISTS employee'''
+        cursor.execute(sql_query)
+        sql_query = ''' DROP USER IF EXISTS boss'''
+        cursor.execute(sql_query)
         sql_query = ''' CREATE database tatadb ''';    
         cursor.execute(sql_query)
         # print("DATABASE CREATED")
@@ -18,6 +23,7 @@ def main(usr, psswd):
         
     except psycopg2.OperationalError as e:
         print("INVALID CREDENTIALS")
+        messagebox.showerror("TATA Motors Database", "INVALID CREDENTIALS\n\n{}".format(e))
         exit()
     except psycopg2.Error as e:
         # print("!!!!!!!!!!!!!CREATE DATABASE UNSUCCESSFUL!!!!!!!!!!!!!")
--- main/view.py
+++ main/view.py
@@ -1,17 +1,18 @@
 import psycopg2
-import getpass
+import sys
+from tkinter import messagebox
+import tkinter as tk
 
-def display():
-    print("SELECT TABLE TO VIEW:")
-    print("(1)Client")
-    print("(2)Employee")
-    print("(3)Department")
-    print("(4)Parts Inventory")
-    print("(5)Sales")
-    print("(6)Supplier")
-    print("(7)Vehicle")
-    print("(8)Model")
-    print("(9)Factory")
+def display(root,usr,psswd):
+    message_label = tk.Label(root, text="SELECT TABLE TO VIEW")
+    message_label.pack(pady=10)
+    selected_option = tk.StringVar()
+    options = ["Client", "Employee", "Model", "Parts Inventory", "Sales", "Supplier", "Vehicle", "Factory", "Department"]
+    selected_option.set(options[0])
+    option_menu = tk.OptionMenu(root, selected_option, *options)
+    option_menu.pack(pady=20)
+    submit_button = tk.Button(root, text="Submit", command=lambda: view(selected_option.get(),usr,psswd))
+    submit_button.pack(pady=20)
 
 def client():
     sql_query = """SELECT * FROM client;"""
@@ -41,57 +42,93 @@ def factory():
     sql_query = """SELECT * FROM factory;"""
     return sql_query
 
-def view(usr,psswd):
+def view(option,usr,psswd):
     try:
         with psycopg2.connect(database='tatadb',
                             host="localhost",
                             user=usr,
                             password=psswd,
                             port=5432) as connection:
-            display()
-            i = input("Choose index : ")
+            # display()
+            # i = input("Choose index : ")
             sql_query = ""
-            match i:
-                case "1": 
+            match option:
+                case "Client": 
                     sql_query = client()
-                case "2": 
+                    output = "ID, NAME, COMPANY, EMAIL"
+                case "Employee": 
                     sql_query = employee()
-                case "3": 
+                    output = "ID, FNAME, LNAME, EMAIL, PHONE, ADDRESS, DESIGN, SALARY, DEPARTMENT"
+                case "Department": 
                     sql_query = department()
-                case "4": 
+                    output = "ID, DEPARTMENT"
+                case "Parts Inventory": 
                     sql_query = parts()
-                case "5": 
+                    output = "ID, NAME, SUPPLIER, EMPLOYEE, QUANTITY, PRICE"
+                case "Sales": 
                     sql_query = sales()
-                case "6": 
+                    output = "ID, CLIENT, EMPLOYEE, VEHICLE, DATE, PRICE, STATUS"
+                case "Supplier": 
                     sql_query = supplier()
-                case "7": 
+                    output = "ID, NAME, COMPANY, EMAIL"
+                case "Vehicle": 
                     sql_query = vehicle()
-                case "8": 
+                    output = "ID, MODEL, COLOR, YEAR, PRICE"
+                case "Model": 
                     sql_query = model()
-                case "9": 
+                    output = "ID, NAME, YEAR, ENGINE, FUEL, DIMENSION, 0-60, KM/L"
+                case "Factory": 
                     sql_query = factory()
+                    output = "EMPLOYEE, VEHICLE, WORK"
                 case _:
                     print("INVALID INPUT!!")
+                    messagebox.showerror("as", "sdf")
                     exit()
 
             with connection.cursor() as cursor:
                 cursor.execute(sql_query)
                 op = cursor.fetchall()
-                for o in op:
-                    print(o)
+                output = output + "\n"
+                for r in op:
+                    for c in r:
+                        output = output + str(c) + ", "
+                    output = output + "\n"
+                messagebox.showinfo(option, output)
                 connection.commit()
                 cursor.close()
         connection.close()
     except psycopg2.OperationalError as e:
         print("INVALID CREDENTIALS")
+        messagebox.showerror("TATA Motors Database", "INVALID CREDENTIALS\n{}".format(e))
         # print(e)
     except psycopg2.Error as e:
         print(e)
+        messagebox.showerror("TATA Motors Database", "INVALID\n{}".format(e))
         connection.rollback()
-
-def main():
-    usr = input("Enter username (usually postgres) : ")
-    psswd = getpass.getpass("Enter password : ")
-    view(usr,psswd)
+        
+def are_credentials_valid(usr, psswd):
+    try:
+        with psycopg2.connect(database='tatadb', host="localhost", user=usr, password=psswd, port=5432):
+            return True
+    except psycopg2.OperationalError:
+        return False
+    
+def main(usr, psswd):
+    if are_credentials_valid(usr, psswd):
+        root = tk.Tk()
+        root.title("VIEW DATABASE")
+        root.geometry("500x300")
+        display(root,usr,psswd)
+        root.mainloop()
+    else:
+        print("INVALID CREDENTIALS")
+        messagebox.showerror("TATA Motors Database", "INVALID CREDENTIALS")
 
 # main()
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        messagebox.showinfo("TATA Motors Database", "FILL THE FIELDS")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        main(username, password)
